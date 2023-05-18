3286389bab7d8be59b5442ce7f0d849f70e9ef46
gabrielnobrel <gabriel_nobresantos@hotmail.com>
2023-05-18 17:47:02 -0300

    edição de localhost

--- src/services/api.js
+++ src/services/api.js
@@ -2,5 +2,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://rocketnotes-api-cadastro.herokuapp.com", //o que mais se repete
+  baseURL: "https://rocketnotes-api-cadastro.herokuapp.com",
 });
